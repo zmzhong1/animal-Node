5a0345d959d3fbfd8800c6c75cc81ef1e4f31a14
Zhao-Ming Zhong <zhongzm8e@gmail.com>
2022-07-25 04:40:32 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# june2022animals
---
+++ joe.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//middleware for find static assets
+app.use(express.static("public"));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allRoutes = require("./controllers")
+app.use(allRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("listenin to port " + 3000);
+});
+
+
